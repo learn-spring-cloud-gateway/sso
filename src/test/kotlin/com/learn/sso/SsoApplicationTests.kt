eef89cec9cfacc917d37658dea3ded9f59eb8969
aassalax <aassali.dev@gmail.com>
2024-07-11 13:30:47 +0200

    first commit

---
+++ src/test/kotlin/com/learn/sso/SsoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.learn.sso
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SsoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
